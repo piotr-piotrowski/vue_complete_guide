7ffdc5d093473d35d6940c475cf0f34285a827bc
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2024-07-25 17:56:06 +0200

    Section 14: Animations & Transitions. 195. Animation Basics & CSS Transitions

--- section14_animations_and_transitions/animations-01-starting-setup/src/App.vue
+++ section14_animations_and_transitions/animations-01-starting-setup/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{ animate: animatedBlock }"></div>
+    <button @click="animateBlock">Animate</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -10,12 +10,15 @@
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
-</template>  
+</template>
 
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return {
+      dialogIsVisible: false,
+      animatedBlock: false
+    };
   },
   methods: {
     showDialog() {
@@ -23,6 +26,9 @@ export default {
     },
     hideDialog() {
       this.dialogIsVisible = false;
+    },
+    animateBlock() {
+      this.animatedBlock = true;
     },
   },
 };
@@ -32,12 +38,15 @@ export default {
 * {
   box-sizing: border-box;
 }
+
 html {
   font-family: sans-serif;
 }
+
 body {
   margin: 0;
 }
+
 button {
   font: inherit;
   padding: 0.5rem 2rem;
@@ -47,17 +56,21 @@ button {
   color: white;
   cursor: pointer;
 }
+
 button:hover,
 button:active {
   background-color: #a80b48;
   border-color: #a80b48;
 }
+
 .block {
   width: 8rem;
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  transition: transform 0.3s ease-out;
 }
+
 .container {
   max-width: 40rem;
   margin: 2rem auto;
@@ -68,5 +81,9 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+
+.animate {
+  transform: translateX(-150px);
 }
 </style>
