6661083ba3f22806ec75daa32b78bb61e30971d5
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2024-07-25 22:31:56 +0200

    Section 14: Animations & Transitions. 202. Example: Animating a Modal

--- section14_animations_and_transitions/animations-01-starting-setup/src/components/BaseModal.vue
+++ section14_animations_and_transitions/animations-01-starting-setup/src/components/BaseModal.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="backdrop" @click="$emit('close')"></div>
-  <dialog open>
-    <slot></slot>
-  </dialog>
+  <div v-if="open" class="backdrop" @click="$emit('close')"></div>
+  <transition name="modal">
+    <dialog open v-if="open">
+      <slot></slot>
+    </dialog>
+  </transition>
 </template>
 
 <script>
 export default {
+  props: ['open'],
   emits: ['close'],
 };
 </script>
@@ -35,4 +38,33 @@ dialog {
   z-index: 100;
   border: none;
 }
+
+.modal-enter-from {}
+
+.modal-enter-active {
+  animation: modal 0.3s ease-out;
+}
+
+.modal-enter-to {}
+
+.modal-leave-from {}
+
+.modal-leave-active {
+  animation: modal 0.3s ease-in reverse;
+}
+
+.modal-leave-to {}
+
+@keyframes modal {
+  from {
+    opacity: 0;
+    transform: translateY(-50px) scale(0.9);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0) scale(1);
+  }
+}
+
 </style>
