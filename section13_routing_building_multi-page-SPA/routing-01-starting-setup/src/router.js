e62341a3f0a8f8047b23b937cea8402848a6ddb6
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2024-07-25 12:10:47 +0200

    Section 13: Routing: Building "Multi-Page" Single Page Applications

---
+++ section13_routing_building_multi-page-SPA/routing-01-starting-setup/src/router.js
@@ -0,0 +1,69 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import TeamsList from './pages/TeamsList.vue';
+import UsersList from './pages/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import TeamsFooter from './pages/TeamsFooter.vue';
+import UsersFooter from './pages/UsersFooter.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/teams' },
+        {
+            name: 'teams',
+            path: '/teams',
+            meta: { needsAuth: true },
+            components: { default: TeamsList, footer: TeamsFooter },
+            children: [
+                { name: 'team-members', path: ':teamId', component: TeamMembers, props: true }
+            ]
+        }, // alias also can be added
+        // { path: '/teams/:teamId', component: TeamMembers, props: true},
+        {
+            path: '/users', components: {
+                default: UsersList, footer: UsersFooter
+            },
+            beforeEnter: (to, from, next) => {
+                console.log('users beforeEnter');
+                console.log(to, from);
+                next();
+            }
+        },
+        { path: '/:notFound(.*)', redirect: '/teams' } // or redirect to NotFound component
+    ],
+    linkActiveClass: 'active',
+    scrollBehavior(_, _2, savedPosition) {
+        // console.log(to, from, savedPosition);
+        if (savedPosition) {
+            return savedPosition;
+        }
+        return { left: 0, top: 0 };
+    }
+});
+
+// before each navigation action
+router.beforeEach((to, from, next) => {
+    console.log('Global beforeEach');
+    console.log(to, from);
+    if (to.meta.needsAuth) {
+        console.log('Needs auth!');
+        next();
+    } else {
+        next();
+    }
+    // if (to.name === 'team-members') {
+    //     next();
+    // } else {
+    //     next({ name: 'team-members', params: { teamId: 't2' } });
+    // }
+    next();
+});
+
+router.afterEach(function (to, from) {
+    // sending analytics data
+    console.log("Global after each");
+    console.log(to, from);
+});
+
+export default router;
