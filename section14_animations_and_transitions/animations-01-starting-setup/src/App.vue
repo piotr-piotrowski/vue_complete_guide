a67d2b9033736e8f8b4bcec999cf3c8f0fc78f59
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-07-27 23:12:58 +0200

    Section 14: Animations & Transitions. 210. Animating Route Changes

--- section14_animations_and_transitions/animations-01-starting-setup/src/App.vue
+++ section14_animations_and_transitions/animations-01-starting-setup/src/App.vue
@@ -1,39 +1,13 @@
 <template>
-  <div class="container">
-    <users-list></users-list>
-  </div>
-  <div class="container">
-    <div class="block" :class="{ animate: animatedBlock }"></div>
-    <button @click="animateBlock">Animate</button>
-  </div>
-  <div class="container">
-    <transition :css="false" name="para" @before-enter="beforeEnter" @before-leave="beforeLeave" @enter="enter"
-      @after-enter="afterEnter" @leave="leave" @after-leave="afterLeave" @enter-cancelled="enterCancelled"
-      @leave-cancelled="leaveCancelled">
-      <p v-if="paraIsVisible">This is only sometimes visible.</p>
+  <router-view v-slot="slotProps">
+    <transition name="route" mode="out-in">
+      <component :is="slotProps.Component"></component>
     </transition>
-    <button @click="toggleParagraph">Toggle Paragraph</button>
-  </div>
-  <div class="container">
-    <transition name="fade-button" mode="out-in">
-      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
-      <button @click="hideUsers" v-else>Hide Users</button>
-    </transition>
-  </div>
-  <base-modal @close="hideDialog" :open="dialogIsVisible">
-    <p>This is a test dialog!</p>
-    <button @click="hideDialog">Close it!</button>
-  </base-modal>
-  <div class="container">
-    <button @click="showDialog">Show Dialog</button>
-  </div>
+  </router-view>
 </template>
 
 <script>
-import UsersList from './components/UsersList.vue';
-
 export default {
-  components: { UsersList },
   data() {
     return {
       dialogIsVisible: false,
@@ -174,6 +148,17 @@ button:active {
 .animate {
   /* transform: translateX(-150px); */
   animation: slide-scale 0.3s ease-out forwards;
+}
+
+.route-enter-from {}
+.route-enter-active {
+  animation: slide-scale 0.4s ease-out;
+}
+
+.route-enter-to {}
+
+.route-leave-active {
+  animation: slide-scale 0.4s ease-in;
 }
 
 @keyframes slide-scale {
