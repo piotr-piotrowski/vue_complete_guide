12a58d22edd164712c7a398c3a82fe5d418f0596
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-07-28 20:38:51 +0200

    Section 15: Vuex. 225. Understanding Local Module State

--- section15_vuex/vuex-01-starting-setup/src/main.js
+++ section15_vuex/vuex-01-starting-setup/src/main.js
@@ -14,6 +14,7 @@ const counterModule = {
             state.counter = state.counter + 2;
         },
         increase(state, payload) {
+            console.log(state);
             state.counter = state.counter + payload.value;
         },
     },
@@ -30,6 +31,10 @@ const counterModule = {
         },
     },
     getters: {
+        // it won't work - state is local
+        // testAuth(state) {
+        //     return state.isLoggedIn
+        // },
         finalCounter(state) {
             return state.counter * 2;
         },
