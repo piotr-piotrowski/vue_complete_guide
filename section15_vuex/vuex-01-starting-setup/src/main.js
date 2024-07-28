3fed4bc0ce157a01c5e05381e54aa64ba145b9c2
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-07-28 20:24:24 +0200

    Section 15: Vuex. 223. Example: Adding More State

--- section15_vuex/vuex-01-starting-setup/src/main.js
+++ section15_vuex/vuex-01-starting-setup/src/main.js
@@ -7,6 +7,7 @@ const store = createStore({
     state() {
         return {
             counter: 0,
+            isLoggedIn: false,
         };
     },
     mutations: {
@@ -15,6 +16,9 @@ const store = createStore({
         },
         increase(state, payload) {
             state.counter = state.counter + payload.value;
+        },
+        setAuth(state, payload) {
+            state.isLoggedIn = payload.isAuth;
         }
     },
     actions: {
@@ -27,6 +31,12 @@ const store = createStore({
         increase(context, payload) {
             console.log(context);
             context.commit('increase', payload);
+        },
+        login(context) {
+            context.commit('setAuth', { isAuth: true });
+        },
+        logout(context) {
+            context.commit('setAuth', { isAuth: false });
         }
     },
     getters: {
@@ -43,6 +53,9 @@ const store = createStore({
             }
             return finalCounter;
         },
+        userIsAuthenticated(state) {
+            return state.isLoggedIn;
+        }
     }
 });
 
