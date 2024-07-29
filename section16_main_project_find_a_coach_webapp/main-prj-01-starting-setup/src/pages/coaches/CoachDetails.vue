f1172081addf6d533115bf5c56cef7e00cfc1d3c
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-07-29 19:21:52 +0200

    Section 16: Main Project. 239. Adding Route Page Components

---
+++ section16_main_project_find_a_coach_webapp/main-prj-01-starting-setup/src/pages/coaches/CoachDetails.vue
@@ -0,0 +1,9 @@
+<template>
+    <h2>CoachDetails</h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ section16_main_project_find_a_coach_webapp/main-prj-01-starting-setup/src/pages/requests/ContactCoach.vue
@@ -0,0 +1,9 @@
+<template>
+    <h2>Contact Coach</h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
