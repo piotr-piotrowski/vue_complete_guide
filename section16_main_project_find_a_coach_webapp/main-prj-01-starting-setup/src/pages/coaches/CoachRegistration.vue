fb659681e7ddf6fe5b853eb34fa9fcc39922ce68
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-07-29 22:36:34 +0200

    Section 16: Main Project. 247. Registering as a Coach: The Form

--- section16_main_project_find_a_coach_webapp/main-prj-01-starting-setup/src/pages/coaches/CoachRegistration.vue
+++ section16_main_project_find_a_coach_webapp/main-prj-01-starting-setup/src/pages/coaches/CoachRegistration.vue
@@ -1,9 +1,18 @@
 <template>
-    <h2>CoachRegistration</h2>
+    <section>
+        <base-card>
+            <h2>Register as a coach now!</h2>
+            <coach-form></coach-form>
+        </base-card>
+    </section>
 </template>
 
 <script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
 export default {
-    
+    components: {
+        CoachForm,
+    },
 }
 </script>
