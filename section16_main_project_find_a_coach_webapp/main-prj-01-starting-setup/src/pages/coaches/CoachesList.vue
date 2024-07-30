6e7c2a2ac45167a232d0362db2d3c7126344b10d
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-07-30 12:55:26 +0200

    Section 16: Main Project. 256. Rendering a Loading Spinner

--- section16_main_project_find_a_coach_webapp/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ section16_main_project_find_a_coach_webapp/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -6,9 +6,12 @@
         <base-card>
             <div class="controls">
                 <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
-                <base-button v-if="!isCoach" link to="/register">Register a Coach</base-button>
+                <base-button v-if="!isCoach && !isLoading" link to="/register">Register a Coach</base-button>
             </div>
-            <ul v-if="hasCoaches">
+            <div v-if="isLoading">
+                <base-spinner></base-spinner>
+            </div>
+            <ul v-else-if="hasCoaches">
                 <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id"
                     :first-name="coach.firstName" :last-name="coach.lastName" :rate="coach.hourlyRate"
                     :areas="coach.areas"></coach-item>
@@ -28,6 +31,7 @@ export default {
     },
     data() {
         return {
+            isLoading: false,
             activeFilters: {
                 frontend: true,
                 backend: true,
@@ -36,6 +40,9 @@ export default {
         };
     },
     computed: {
+        isCoach() {
+            return this.$store.getters['coaches/isCoach'];
+        },
         filteredCoaches() {
             const coaches = this.$store.getters['coaches/coaches'];
             return coaches.filter(coach => {
@@ -51,10 +58,7 @@ export default {
             });
         },
         hasCoaches() {
-            return this.$store.getters['coaches/hasCoaches'];
-        },
-        isCoach() {
-            return this.$store.getters['coaches/isCoach'];
+            return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
         },
 
     },
@@ -65,8 +69,10 @@ export default {
         setFilters(updatedFilters) {
             this.activeFilters = updatedFilters;
         },
-        loadCoaches() {
-            this.$store.dispatch('coaches/loadCoaches');
+        async loadCoaches() {
+            this.isLoading = true;
+            await this.$store.dispatch('coaches/loadCoaches');
+            this.isLoading = false;
         }
     }
 }
