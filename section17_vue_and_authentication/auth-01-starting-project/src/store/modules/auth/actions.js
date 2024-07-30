b596aaee22ac2cab15e6f7d7ef0c0173550e6093
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-07-30 21:37:37 +0200

    Section 17: Vue & Authentication. 273. Adding a "Logout" Action & Flow

--- section17_vue_and_authentication/auth-01-starting-project/src/store/modules/auth/actions.js
+++ section17_vue_and_authentication/auth-01-starting-project/src/store/modules/auth/actions.js
@@ -51,5 +51,12 @@ export default {
       userId: responseData.localId,
       tokenExpiration: responseData.expiresIn
     });
+  },
+  logout(context) {
+    context.commit('setUser', {
+      token: null,
+      userId: null,
+      tokenExpiration: null
+    });
   }
 };
