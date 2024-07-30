719ef2ca8f8ea5a8be83b255a12548fe4629599d
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-07-30 09:34:07 +0200

    Section 16: Main Project. 251. Storing Requests (Messages) With Vuex

--- section16_main_project_find_a_coach_webapp/main-prj-01-starting-setup/src/pages/requests/ContactCoach.vue
+++ section16_main_project_find_a_coach_webapp/main-prj-01-starting-setup/src/pages/requests/ContactCoach.vue
@@ -2,7 +2,7 @@
     <form @submit.prevent="submitForm">
         <div class="form-control">
             <label for="email">Your E-Mail</label>
-            <input type="email" id="email" v-model.trim="email"/>
+            <input type="email" id="email" v-model.trim="email" />
         </div>
         <div class="form-control">
             <label for="message">Message</label>
@@ -32,7 +32,12 @@ export default {
                 return;
             }
 
-            
+            this.$store.dispatch('requests/contactCoach', {
+                email: this.email,
+                message: this.message,
+                coachId: this.$route.params.id
+            });
+            this.$router.replace('/coaches');
         },
 
     }
@@ -41,44 +46,44 @@ export default {
 
 <style scoped>
 form {
-  margin: 1rem;
-  border: 1px solid #ccc;
-  border-radius: 12px;
-  padding: 1rem;
+    margin: 1rem;
+    border: 1px solid #ccc;
+    border-radius: 12px;
+    padding: 1rem;
 }
 
 .form-control {
-  margin: 0.5rem 0;
+    margin: 0.5rem 0;
 }
 
 label {
-  font-weight: bold;
-  margin-bottom: 0.5rem;
-  display: block;
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+    display: block;
 }
 
 input,
 textarea {
-  display: block;
-  width: 100%;
-  font: inherit;
-  border: 1px solid #ccc;
-  padding: 0.15rem;
+    display: block;
+    width: 100%;
+    font: inherit;
+    border: 1px solid #ccc;
+    padding: 0.15rem;
 }
 
 input:focus,
 textarea:focus {
-  border-color: #3d008d;
-  background-color: #faf6ff;
-  outline: none;
+    border-color: #3d008d;
+    background-color: #faf6ff;
+    outline: none;
 }
 
 .errors {
-  font-weight: bold;
-  color: red;
+    font-weight: bold;
+    color: red;
 }
 
 .actions {
-  text-align: center;
+    text-align: center;
 }
 </style>
--- section16_main_project_find_a_coach_webapp/main-prj-01-starting-setup/src/store/index.js
+++ section16_main_project_find_a_coach_webapp/main-prj-01-starting-setup/src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 
-import coachesMoudules from './modules/coaches/index.js';
+import coachesModule from './modules/coaches/index.js';
+import requestsModule from './modules/requests/index.js';
 
 const store = createStore({
     modules: {
-        coaches: coachesMoudules,
+        coaches: coachesModule,
+        requests: requestsModule
     },
     state() {
         return {
