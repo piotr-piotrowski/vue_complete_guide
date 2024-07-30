c46041623aba3328af6e9f9303b25716a2839373
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-07-30 20:50:32 +0200

    Section 17: Vue & Authentication. 270. Adding a "Login" Action & Flow

--- section17_vue_and_authentication/auth-01-starting-project/src/store/modules/auth/actions.js
+++ section17_vue_and_authentication/auth-01-starting-project/src/store/modules/auth/actions.js
@@ -1,7 +1,33 @@
 export default {
-  login() { },
+  async login(context, payload) {
+    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=xxx', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    });
+
+    const responseData = response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(responseData.message || 'Failed to authenticate.');
+      throw error;
+    }
+
+    console.log(responseData);
+
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn
+    });
+
+  },
   async signup(context, payload) {
-    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB5h43IhYSqArEne6rf0ItneJAP71n503c ', {
+    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=xxx', {
       method: 'POST',
       body: JSON.stringify({
         email: payload.email,
