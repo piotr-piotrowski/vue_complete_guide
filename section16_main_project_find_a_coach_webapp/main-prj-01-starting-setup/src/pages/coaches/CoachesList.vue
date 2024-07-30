59168192fdcadd849dd330d17abbf16b1eec2a91
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-07-30 14:38:10 +0200

    Section 16: Main Project. 257. Adding Http Error Handling

--- section16_main_project_find_a_coach_webapp/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ section16_main_project_find_a_coach_webapp/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -1,4 +1,7 @@
 <template>
+    <base-dialog :show="!!error" title="An error occured!" @close="handleError">
+        <p>{{ error }}</p>
+    </base-dialog>
     <section>
         <coach-filter @change-filter="setFilters"></coach-filter>
     </section>
@@ -24,6 +27,7 @@
 <script>
 import CoachItem from '../../components/coaches/CoachItem.vue';
 import CoachFilter from '../../components/coaches/CoachFilter.vue';
+import { handleError } from 'vue';
 
 export default {
     components: {
@@ -32,6 +36,7 @@ export default {
     data() {
         return {
             isLoading: false,
+            error: null,
             activeFilters: {
                 frontend: true,
                 backend: true,
@@ -71,8 +76,15 @@ export default {
         },
         async loadCoaches() {
             this.isLoading = true;
-            await this.$store.dispatch('coaches/loadCoaches');
+            try {
+                await this.$store.dispatch('coaches/loadCoaches');
+            } catch (error) {
+                this.error = error.message || 'Something went wrong!';
+            }
             this.isLoading = false;
+        },
+        handleError() {
+            this.error = null;
         }
     }
 }
