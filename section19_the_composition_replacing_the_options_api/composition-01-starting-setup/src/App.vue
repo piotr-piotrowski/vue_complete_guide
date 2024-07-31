8a0a67d1ee601e4a567119bbe84c100abea79cf6
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-07-31 11:02:44 +0200

    Section 19: The Composition API - Replacing the Options API. 291. Reactivity: A Deep Dive

--- section19_the_composition_replacing_the_options_api/composition-01-starting-setup/src/App.vue
+++ section19_the_composition_replacing_the_options_api/composition-01-starting-setup/src/App.vue
@@ -2,23 +2,30 @@
   <section class="container">
     <h2>{{ user.name }}</h2>
     <h3>{{ user.age }}</h3>
+    <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
   </section>
 </template>
 
 <script>
 // import { ref } from 'vue';
-import { reactive } from 'vue';
+import { reactive, ref, isRef, isReactive, toRefs } from 'vue';
 
 export default {
   setup() {
     // ref for any value
     // const uName = ref('Maximilian');
-    // const uAge = ref(31);
+    const uAge = ref(31);
     // reactive for objects
     const user = reactive({
       name: 'Maximilian',
       age: 31
     });
+
+    console.log(isRef(uAge.value));
+    console.log(isRef(uAge));
+    console.log(isReactive(user.name), user.age);
+    console.log(isReactive(user));
 
     setTimeout(function () {
       // for distinct values
@@ -32,8 +39,11 @@ export default {
       user.age = 32;
     }, 2000);
 
+    const userRefs = toRefs(user);
+
     // return { userName: user.value.name, age: user.value.age, user: user };
-    return { user: user };
+    // return { user: user };
+    return { user: user, userName: userRefs.name, age: userRefs.age };
   },
   // data() {
   //   return {
