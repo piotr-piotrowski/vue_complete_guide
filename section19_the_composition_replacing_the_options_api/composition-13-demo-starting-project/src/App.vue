9d10784976fd5a7cbe3fef1ff372eca4bd87520f
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-07-31 13:38:56 +0200

    Section 19: The Composition API - Replacing the Options API. 303. Migrating a First Component

--- section19_the_composition_replacing_the_options_api/composition-13-demo-starting-project/src/App.vue
+++ section19_the_composition_replacing_the_options_api/composition-13-demo-starting-project/src/App.vue
@@ -10,23 +10,23 @@ import USER_DATA from './dummy-data.js';
 
 import UserList from './components/users/UserList.vue';
 import ProjectsList from './components/projects/ProjectsList.vue';
+import { ref } from 'vue';
 
 export default {
   components: {
     UserList,
     ProjectsList,
   },
-  data() {
-    return {
-      selectedUser: null,
-      activeUsers: USER_DATA,
-    };
-  },
-  methods: {
-    selectUser(uid) {
-      this.selectedUser = this.activeUsers.find((usr) => usr.id === uid);
-    },
-  },
+  setup() {
+    const selectedUser = ref(null);
+    const activeUsers = USER_DATA;
+
+    function selectUser(uid) {
+      selectedUser.value = activeUsers.find((usr) => usr.id === uid);
+    } 
+
+    return { selectedUser, activeUsers, selectUser };
+  }
 };
 </script>
 
@@ -34,9 +34,11 @@ export default {
 * {
   box-sizing: border-box;
 }
+
 html {
   font-family: sans-serif;
 }
+
 body {
   margin: 0;
 }
@@ -55,6 +57,7 @@ button {
   cursor: pointer;
   margin: 0.5rem 0.5rem 0.5rem 0;
 }
+
 button:hover,
 button:active {
   background-color: #efefff;
