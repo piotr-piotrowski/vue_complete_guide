7c6141204e092cc97af090d257f41c2fb832716b
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-07-31 15:48:15 +0200

    Section 19: The Composition API - Replacing the Options API. 307. The Route & Router Objects and the Composition API

--- section19_the_composition_replacing_the_options_api/composition-17-routing-starting-project/src/pages/ProductDetails.vue
+++ section19_the_composition_replacing_the_options_api/composition-17-routing-starting-project/src/pages/ProductDetails.vue
@@ -9,17 +9,18 @@
 
 <script>
 import { inject, computed } from 'vue';
+import { useRoute } from 'vue-router';
 
 export default {
   props: ['pid'],
-  setup(props) {
+  setup() {
 
-    const products = inject('products')
-    // const title = ref('');
-    // const price = ref(null);
-    // const description = ref('');
+    const products = inject('products');
 
-    const selectedProduct = computed(() => products.value.find(product => product.id === props.pid));
+    const route = useRoute();
+    console.log(route);
+
+    const selectedProduct = computed(() => products.value.find(product => product.id === route.params.pid));
     const title = computed(() => selectedProduct.value.title);
     const price = computed(() => selectedProduct.value.price);
     const description = computed(() => selectedProduct.value.description);
