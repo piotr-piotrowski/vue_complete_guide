bd691a7efec83931a4c6296780f15b8c413f08de
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-07-31 12:07:06 +0200

    Section 19: The Composition API - Replacing the Options API. 293. Replacing "Computed Properties" with the "computed" Function

--- section19_the_composition_replacing_the_options_api/composition-01-starting-setup/src/App.vue
+++ section19_the_composition_replacing_the_options_api/composition-01-starting-setup/src/App.vue
@@ -1,33 +1,48 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
-    <h3>{{ user.age }}</h3>
+    <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
     <button @click="setAge">Change Age</button>
+    <div>
+      <input type="text" placeholder="First Name" @input="setFirstName" />
+      <input type="text" placeholder="Last Name" @input="setLastName" />
+    </div>
   </section>
 </template>
 
 <script>
-// import { ref } from 'vue';
-import { reactive } from 'vue';
+import { ref, computed } from 'vue';
+// import { reactive } from 'vue';
 
 export default {
   setup() {
-    const user = reactive({
-      name: 'Maximilian',
-      age: 31
+    // const uName = ref('Maximilian');
+    const firstName = ref('');
+    const lastName = ref('');
+    const uAge = ref(31);
+    // const user = reactive({
+    //   name: 'Maximilian',
+    //   age: 31
+    // });
+
+    const uName = computed(function() {
+      return firstName.value + ' ' + lastName.value;
     });
 
     function setNewAge() {
-      user.age = 32;
+      uAge.value = 32;
     }
 
-    return { user: user, setAge: setNewAge };
+    function setFirstName(event) {
+      firstName.value = event.target.value;
+    }
+
+    function setLastName(event) {
+      lastName.value = event.target.value;
+    }
+
+    return { userName: uName, age: uAge, setAge: setNewAge, setFirstName, setLastName };
   },
-  // data() {
-  //   return {
-  //     userName: 'Maximilian',
-  //   };
-  // },
 };
 </script>
 
