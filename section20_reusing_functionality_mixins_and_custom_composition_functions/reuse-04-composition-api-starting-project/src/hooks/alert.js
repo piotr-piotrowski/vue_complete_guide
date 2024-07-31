06f5b5a46d910741a8a245d0753c9a2791091abc
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-07-31 16:29:55 +0200

    Section 20: Reusing Functionality. Section 317. Custom Hooks / Composables & The Composition API

---
+++ section20_reusing_functionality_mixins_and_custom_composition_functions/reuse-04-composition-api-starting-project/src/hooks/alert.js
@@ -0,0 +1,18 @@
+const { ref } = require("vue");
+
+export default function useAlert() {
+    const alertIsVisible = ref(false);
+
+    function showAlert() {
+        alertIsVisible.value = true;
+    }
+    function hideAlert() {
+        alertIsVisible.value = false;
+    }
+
+    return [
+        alertIsVisible,
+        showAlert,
+        hideAlert
+    ];
+}
